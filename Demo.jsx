9a05846c72e56e8062682dcbe1de342197db947e
Gajanandyadav1 <132254405+Gajanandyadav1@users.noreply.github.com>
2025-06-24 10:50:16 +0530

    Update Demo.jsx

--- Demo.jsx
+++ Demo.jsx
@@ -1,5 +1,5 @@
 import {  useRef, useState } from "react";
-import { Paperclip, Mic, Send } from "lucide-react";
+import { Paperclip, Mic, Send } from "lucide-react" ;
 
 const ChatApp = () => {
   const [messages, setMessages] = useState([]);
